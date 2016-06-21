2eef75a9c9997889bd71fb2ce7c631b2dd80f384
Faisal Mahmood <fahmahmood@gmail.com>
2016-06-21 15:45:15 +0100

    airport challenge complete

--- src/Plane.js
+++ src/Plane.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 function Plane() {
 }
 Plane.prototype.land = function(airport){
@@ -12,38 +10,3 @@ this._location = airport;
 Plane.prototype.takeoff = function(airport){
 this._location.clearForTakeOff('');
 };
-
-// FizzBuzz.prototype.play = function(number) {
-
-// // return this
-
-// if (this.isDivisibleBy(15, number)) {
-//     return 'fizzbuzz';
-//  }
-
-// if (this.isDivisibleBy(5, number)) {
-// 		return 'buzz';
-// }
-
-// if (this.isDivisibleBy(3, number)) {
-// 		return 'fizz';
-// }
-
-// return number;
-
-// };
-
-
-// FizzBuzz.prototype.isDivisibleBy = function(divisor, number) {
-// 	return number % divisor === 0
-// }
-
-// //  !!!!!!!!!!!!!!!!!
-// // 
-// // To see what you're doing in the console run this code
-// // FizzBuzz.prototype.printOneToHundred = function() {
-// // for (var i = 1; i <= 100; i++) {
-// //   console.log(this.play(i));
-// // } 
-// // }
-// // !!!!!!!!!!!!
